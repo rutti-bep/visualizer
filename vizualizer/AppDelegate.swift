22eaed0c603ffa7df38bfae9ff3fbf42fe1ab6c7
rutti-bep <konnosatoru@konnoakira-no-macbook.local>
2017-06-22 21:25:15 +0900

    Initial Commit

---
+++ vizualizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vizualizer
+//
+//  Created by 今野暁 on 2017/06/22.
+//  Copyright © 2017年 今野暁. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ vizualizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vizualizer
+//
+//  Created by 今野暁 on 2017/06/22.
+//  Copyright © 2017年 今野暁. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
