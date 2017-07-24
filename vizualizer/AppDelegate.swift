b2c069a3bfabed3ae40cbb5d2b40fc707464212f
rutti-bep <konnosatoru@konnoakira-no-macbook.local>
2017-07-24 22:37:14 +0900

    add graph

--- vizualizer/AppDelegate.swift
+++ vizualizer/AppDelegate.swift
@@ -7,26 +7,51 @@
 //
 
 import Cocoa
+import AppKit
 import AVFoundation
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var window:NSWindow?;
+    var graphView:Graph?;
+    
    // var myAudioPlayer:MyAudioPlayer!
     var audioCatcher:AudioCatcher = AudioCatcher.sharedInstance;
 
     func printBufs(){
-        print(audioCatcher.soundArray);
+        let array = audioCatcher.soundArray;
+        graphView!.array = array
+        graphView!.needsDisplay = true
+        print(array);
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        window = NSWindow(contentRect: NSRect(x:NSScreen.main()!.frame.midX,y:NSScreen.main()!.frame.midY,width:400,height:400), styleMask: [.closable,.titled], backing: NSBackingStoreType.buffered, defer:false)
+        window!.acceptsMouseMovedEvents = true
+        window!.title = "vizualizer"
+        window!.center()
+        window!.isMovableByWindowBackground = true
+        window!.makeKeyAndOrderFront(nil)
+
+        graphView = Graph()
+        graphView!.frame = NSRect(x:0,y:0,width:window!.frame.width,height:window!.frame.height);
+        graphView!.wantsLayer = true
+  //      graphView!.layer?.backgroundColor = NSColor.red.cgColor;
+        window!.contentView?.addSubview(graphView!);
+       
+        graphView!.array = [20000,1500,36000]
+        
+        
         try! print(getInputDevices());
         //fft()
         audioCatcher.initialize();
         audioCatcher.start();
         
-        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.printBufs), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.printBufs), userInfo: nil, repeats: true)
         
     }
     
--- vizualizer/graphDraw.swift
+++ vizualizer/graphDraw.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import AppKit
+import Cocoa
+
+final class Graph: NSView{
+    var lineWidth:Float = 3.0
+    var lineColor:CGColor = CGColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+    var circleWidth:Float = 1.0
+    var circleColor:CGColor = CGColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+    var array = [UInt16]();
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        let path = NSBezierPath()
+        path.move(to: NSPoint(x:self.frame.minX,y:self.frame.minY))
+        
+        let widthSpace = self.frame.width/CGFloat(array.count);
+        for i in 0..<array.count {
+            let height = CGFloat(array[i])/65535.0*self.frame.height;
+            let width = widthSpace*CGFloat(i+1);
+            let point = NSPoint(x:width,y:height)
+            path.line(to: point)
+       //     Swift.print(height)
+       //     Swift.print(point)
+        }
+        path.stroke()
+    }
+    
+}
